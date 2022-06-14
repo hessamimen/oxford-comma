df1e20a18d70840c847fd9d85b0086dedca8f3fc
Hessam Imen <hessam.imen@gmail.com>
2022-06-14 16:59:39 +1000

    Done with assignmnet

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
+# def oxford_comma(array)
+#     array.join
+#     array.join(' and ')
+    
+# end
 
+
+def oxford_comma(array)
+    if array.size == 1
+        array.join
+    elsif array.size == 2
+        array.join(' and ')
+    elsif array.size > 2
+        array[0..-2].join(', ') + ", and " + array[-1]
+    end
+    
 end
